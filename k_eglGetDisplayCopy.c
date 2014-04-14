c9501d1b8372b631f6c5d3ed45a079a6d2e79d8b
Mohamed MEDIOUNI <mohamedmediouni91@gmail.com>
2014-04-14 17:36:33 +0000

    Add license information

--- k_eglGetDisplayCopy.c
+++ k_eglGetDisplayCopy.c
@@ -9,6 +9,24 @@
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <X11/Xutil.h>
+/*  This file is part of X11EGLRPI
+Copyright (C)2014 Mohamed MEDIOUNI
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
 Window* root;
 Window* window;
 EGLDisplay *edisplay;
@@ -103,3 +121,8 @@ EGLBoolean eglSwapBuffers(EGLDisplay edisplay, EGLSurface egsurface) {
 	 free(buffer);
 	 return EGL_TRUE;
 }
+EGLBoolean eglMakeCurrent(EGLDisplay display, EGLSurface draw, EGLSurface read, EGLContext context) {
+	real_eglMakeCurrent(display,draw,read,context);
+	return EGL_TRUE;
+}
+
